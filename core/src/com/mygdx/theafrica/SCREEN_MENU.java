ca0f830991e49c6214d7f84234c91f24cd66dbed
Alvaro Tomas <auberontermeraz@gmail.com>
2019-06-06 15:50:53 +0200

    Ya va el iniciar el juego estando en la arcade

--- core/src/com/mygdx/theafrica/SCREEN_MENU.java
+++ core/src/com/mygdx/theafrica/SCREEN_MENU.java
@@ -3,11 +3,15 @@ package com.mygdx.theafrica;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.controllers.Controller;
+import com.badlogic.gdx.controllers.ControllerListener;
 import com.badlogic.gdx.controllers.Controllers;
+import com.badlogic.gdx.controllers.PovDirection;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Align;
 import com.mygdx.theafrica.HUD.HUD;
 import com.mygdx.theafrica.HUD.TextButton;
@@ -39,7 +43,7 @@ public class SCREEN_MENU implements Screen {
         hudCamera = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 
         hud = new HUD();
-        b1 = new TextButton("START ",Gdx.graphics.getWidth()/2.8f,Gdx.graphics.getHeight()/4,320,70)
+        b1 = new TextButton("START ",Gdx.graphics.getWidth()/2.5f,Gdx.graphics.getHeight()/4,320,70)
         {
             @Override
             public void click() {
@@ -47,6 +51,58 @@ public class SCREEN_MENU implements Screen {
             }
         };
 
+
+        if( Controllers.getControllers().size > 0)
+            Controllers.addListener(new ControllerListener() {
+                @Override
+                public void connected(Controller controller) {
+
+                }
+
+                @Override
+                public void disconnected(Controller controller) {
+
+                }
+
+                @Override
+                public boolean buttonDown(Controller controller, int buttonCode) {
+
+                    if(buttonCode == 0)
+                        game.setScreen(game.gameScreen);
+
+                    return false;
+                }
+
+                @Override
+                public boolean buttonUp(Controller controller, int buttonCode) {
+                    return false;
+                }
+
+                @Override
+                public boolean axisMoved(Controller controller, int axisCode, float value) {
+                    return false;
+                }
+
+                @Override
+                public boolean povMoved(Controller controller, int povCode, PovDirection value) {
+                    return false;
+                }
+
+                @Override
+                public boolean xSliderMoved(Controller controller, int sliderCode, boolean value) {
+                    return false;
+                }
+
+                @Override
+                public boolean ySliderMoved(Controller controller, int sliderCode, boolean value) {
+                    return false;
+                }
+
+                @Override
+                public boolean accelerometerMoved(Controller controller, int accelerometerCode, Vector3 value) {
+                    return false;
+                }
+            });
 
         hud.add(b1);
 
