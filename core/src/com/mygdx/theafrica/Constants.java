2ca714847b1bc1475a7dd73d7f3ca54c4877598a
Alvaro Tomas <auberontermeraz@gmail.com>
2019-06-06 13:52:29 +0200

    Grid positions

--- core/src/com/mygdx/theafrica/Constants.java
+++ core/src/com/mygdx/theafrica/Constants.java
@@ -1,8 +1,8 @@
 package com.mygdx.theafrica;
 
 public class Constants {
-    public final static float VIEWPORT_WIDTH = 30;
-    public final static float VIEWPORT_HEIGHT = 50;
+    public final static float VIEWPORT_WIDTH = 1920;
+    public final static float VIEWPORT_HEIGHT = 1080;
     public static final int SPRITE_NUMBER = 1;
     public static final int PLAYER_TEXREG_NUMBER = 8;
 
--- core/src/com/mygdx/theafrica/Grid.java
+++ core/src/com/mygdx/theafrica/Grid.java
@@ -1,4 +1,33 @@
 package com.mygdx.theafrica;
 
+import com.badlogic.gdx.math.Vector2;
+
 public class Grid {
+
+    int size = 4;
+    int cardSize;
+    int originalX;
+    int originalY;
+
+    public Vector2[][] positions;
+    public boolean[][] isOcuppied;
+
+    public Grid()
+    {
+        positions = new Vector2[size][size];
+        cardSize = 96;
+        originalX = 400;
+        originalY = 800;
+
+        for(int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                positions[i][j].x = originalX + cardSize * j;
+                positions[i][j].y = originalY - cardSize * i;
+                isOcuppied[i][j] = false;
+            }
+        }
+    }
+
 }
