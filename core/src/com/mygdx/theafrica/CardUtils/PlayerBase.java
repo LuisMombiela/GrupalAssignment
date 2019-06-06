b97624c4bef298e0b03eff4511fdfab1ba6a3d23
LuisMombiela <luismombiela@gmail.com>
2019-06-06 17:00:48 +0200

    sdfsdf

--- core/src/com/mygdx/theafrica/CardUtils/PlayerBase.java
+++ core/src/com/mygdx/theafrica/CardUtils/PlayerBase.java
@@ -1,5 +1,6 @@
 package com.mygdx.theafrica.CardUtils;
 
+import com.badlogic.gdx.math.Vector2;
 import com.mygdx.theafrica.Player;
 
 public class PlayerBase {
@@ -10,22 +11,38 @@ public class PlayerBase {
     public boolean hasGreenHouse;
     public boolean hasHospital;
     public boolean hasSchool;
+    public Vector2 greenhousePos; //Pillar la greenhouse pos
+    public Vector2 schoolPos; //Pillar la school pos
+    public Vector2 hospitalPos; //Pillar la hospital pos
 
-    public PlayerBase(int index)
+    public PlayerBase(int playerNumber)
     {
-        this.baseIndex = index;
-
-        //lo que falta
+        this.baseIndex = playerNumber;
+        //Necesito pillar la referencia del player
     }
 
     public boolean canBuild(Hospital h)
     {
         if(myPlayer.currentWood >= h.requiredWood && myPlayer.currentIron >= h.requiredIron && myPlayer.currentBandages >= h.requiredBandages)
             Hospital.build();
+        hasHospital = true;
+        System.out.println("Player tal hospital has been built");
+    }
+    public boolean canBuild(School s)
+    {
+        if(myPlayer.currentWood >= s.requiredWood && myPlayer.currentIron >= s.requiredIron && myPlayer.currentBooks >= s.requiredBooks)
+            School.build();
+        hasSchool = true;
+        System.out.println("Player tal School has been built");
+    }
+    public boolean canBuild(Greenhouse gh)
+    {
+        if(myPlayer.currentWood >= gh.requiredWood && myPlayer.currentIron >= gh.requiredIron && myPlayer.currentBandages >= gh.requiredBandages)
+            Greenhouse.build();
+        hasGreenHouse = true;
+        System.out.println("Player tal Greenhouse has been built");
     }
 
-    //Funciones de añadir las mejoras
-    if
 
 
 }
